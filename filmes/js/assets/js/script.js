96367b457629531b70b0a3ddd3c1c2af126ebdb6
Suzana Sales <98489178+suzana-s@users.noreply.github.com>
2022-07-16 00:55:38 -0300

    Add files via upload

---
+++ filmes/js/assets/js/script.js
@@ -0,0 +1,80 @@
+//SELECIONANDO A LISTA (UL) DO MEU HTML
+const listafilme = document.querySelector(".listafilme")
+
+
+//DECLARANDO FUNÇÃO LISTAR OS FILMES
+function listarFilmes(colecaoFilmes){
+
+        //LIMPAR VITRINE HTML
+        listafilme.innerHTML = ""
+
+        //ACESSANDO A COLEÇÃO DE FILMES(ARRAY) -> PASSANDO PARA FUNÇÃO TEMPLATE
+        colecaoFilmes.forEach(function(filme){
+
+            //PASSANDO CADA UM DOS FILMES => PARA CRIAR O TEMPLATE PARA CADA FILME
+           const template  =  criarTemplate(filme)
+        
+            //ADICIONANDO O FILME DE FORMA VISUAL NO HTML
+            listafilme.appendChild(template)
+
+        })
+
+}
+//CHAMANDO PARA EXECUTAR OS PASSOS
+listarFilmes(filmes)
+
+//FUNÇÃO QUE CRIA O TEMPLATE (LI)
+function criarTemplate(filme){
+
+    //CRIANDO UM TEMPLATE DO TIPO HTML 
+    const li = document.createElement("li")
+    
+    //ADICIONANDO UMA ESTRUTURA HTML PRA DENTRO DO (LI)
+    li.innerHTML = `
+        <figure>
+            <img src="${filme.url}" alt="${filme.nome}" />
+        </figure>
+        <h2>${filme.nome}</h2>
+    `
+    return li
+}
+
+//FILTRAR FILME POR CATEGORIA
+function filtrarFilme(filtroCategoria){
+
+    //LOOP PARA FILTRAR OS FILMES
+    const filmesFiltrados = filmes.filter(function(filme){
+
+        //SE A CATEGORIA BUSCADA FOR === A DO FILME ATUAL MOSTRE O FILME/ OU RETORNE O FIMLE
+        if(filtroCategoria == filme.categoria){
+            //RETORNE O FILME
+            return filme
+        }
+
+    })
+
+    listarFilmes(filmesFiltrados)
+}
+
+// SELECIONANDO MENU 
+const secaocategoria = document.querySelector(".secaocategoria")
+
+//ADICIONANDO UM INTERCEPTADOR DE EVENTO
+secaocategoria.addEventListener("click", function(event){
+    
+    //CANCELAR EVENTO DO LINK => NÃO RECARREGAR A TELA
+    event.preventDefault()
+    
+    //ONDE FOI CLICADO
+    const categoriaSelecionada = event.target.text
+    
+    //VERIFICANDO PARA MOSTRAR TOFDAS
+    if(categoriaSelecionada == "Mostrar todos"){
+    
+        listarFilmes(filmes)
+    }else{
+        filtrarFilme(categoriaSelecionada)
+    }
+    
+})
+
